a0e488f7b755b0b547700a1514057954993a214c
MauVillarreal2005 <mauriciovillarrealrodriguez@gmail.com>
2023-10-20 15:53:11 -0700

    update

--- src/main/java/com/team6647/RobotContainer.java
+++ src/main/java/com/team6647/RobotContainer.java
@@ -16,7 +16,7 @@ import com.team6647.Subsystems.PivotSubsystem;
 import com.team6647.Subsystems.PivotSubsystem.PivotState;
 import com.team6647.Commands.hybrid.ToggleIntake;
 import com.team6647.Commands.hybrid.MovePivot;
-import com.team6647.utils.Constants.DriveConsants;
+import com.team6647.utils.Constants.DriveConstants;
 import com.team6647.utils.Constants.OperatorConstants;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -30,7 +30,6 @@ public class RobotContainer extends SuperRobotContainer {
   private PivotSubsystem pivotSubsystem;
 
   private RobotContainer() {
-    configureBindings();
   }
 
   public static RobotContainer getInstance() {
@@ -43,14 +42,14 @@ public class RobotContainer extends SuperRobotContainer {
   @Override
   public void initSubsystems() {
     andromedaSwerve = AndromedaSwerve.getInstance(new NeoAndromedaModule[] {
-        new NeoAndromedaModule(0, "Front Right", DriveConsants.mod1Const,
-            AndromedaProfileConfig.getConfig(AndromedaProfiles.NEO_CONFIG)),
-        new NeoAndromedaModule(1, "Back Right", DriveConsants.mod2Const,
-            AndromedaProfileConfig.getConfig(AndromedaProfiles.NEO_CONFIG)),
-        new NeoAndromedaModule(2, "Back Left", DriveConsants.mod3Const,
-            AndromedaProfileConfig.getConfig(AndromedaProfiles.NEO_CONFIG)),
-        new NeoAndromedaModule(3, "Front Left", DriveConsants.mod4Const,
-            AndromedaProfileConfig.getConfig(AndromedaProfiles.NEO_CONFIG))
+        new NeoAndromedaModule(0, "Front Right", DriveConstants.mod1Const,
+            DriveConstants.neoAndromedaSwerveConfig()),
+        new NeoAndromedaModule(1, "Back Right", DriveConstants.mod2Const,
+            DriveConstants.neoAndromedaSwerveConfig()),
+        new NeoAndromedaModule(2, "Back Left", DriveConstants.mod3Const,
+            DriveConstants.neoAndromedaSwerveConfig()),
+        new NeoAndromedaModule(3, "Front Left", DriveConstants.mod4Const,
+            DriveConstants.neoAndromedaSwerveConfig())
     }, AndromedaProfileConfig.getConfig(AndromedaProfiles.NEO_CONFIG));
 
     intakeSubsystem = IntakeSubsystem.getInstance();
@@ -67,61 +66,64 @@ public class RobotContainer extends SuperRobotContainer {
             () -> OperatorConstants.driverController1.getRightX(),
             () -> OperatorConstants.driverController1.leftStick().getAsBoolean()));
 
-  
     /* PIVOT LOW */
     OperatorConstants.driverController2.a().whileTrue(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.LOW),
-          Commands.waitSeconds(1)))
+        new MovePivot(pivotSubsystem, PivotState.LOW),
+        Commands.waitSeconds(1)))
 
-      .onFalse(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.HOMED),
-          Commands.waitSeconds(1)));
+        .onFalse(Commands.sequence(
+            new MovePivot(pivotSubsystem, PivotState.HOMED),
+            Commands.waitSeconds(1)));
 
     /* PIVOT MID */
     OperatorConstants.driverController2.b().whileTrue(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.MID),
-          Commands.waitSeconds(1)))
+        new MovePivot(pivotSubsystem, PivotState.MID),
+        Commands.waitSeconds(1)))
 
-      .onFalse(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.HOMED),
-          Commands.waitSeconds(1)));
-    
+        .onFalse(Commands.sequence(
+            new MovePivot(pivotSubsystem, PivotState.HOMED),
+            Commands.waitSeconds(1)));
+
     /* PIVOT HIGH */
     OperatorConstants.driverController2.x().whileTrue(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.HIGH),
-          Commands.waitSeconds(1)))
+        new MovePivot(pivotSubsystem, PivotState.HIGH),
+        Commands.waitSeconds(1)))
 
-      .onFalse(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.HOMED),
-          Commands.waitSeconds(1)));
+        .onFalse(Commands.sequence(
+            new MovePivot(pivotSubsystem, PivotState.HOMED),
+            Commands.waitSeconds(1)));
 
     /* PIVOT COLLECT */
     OperatorConstants.driverController2.y().whileTrue(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.FLOOR),
-          Commands.waitSeconds(1)))
-      .onFalse(Commands.sequence(
-          new MovePivot(pivotSubsystem, PivotState.HOMED),
-          Commands.waitSeconds(1)));
+        new MovePivot(pivotSubsystem, PivotState.FLOOR),
+        Commands.waitSeconds(1)))
+        .onFalse(Commands.sequence(
+            new MovePivot(pivotSubsystem, PivotState.HOMED),
+            Commands.waitSeconds(1)));
 
     /* INTAKE SCORE */
-    /*OperatorConstants.driverController2.rightTrigger().whileTrue(Commands.sequence(
-      switch(getPivState()){
-        case(HOMED):
-          break;
-        case(COLLECT):
-          break;
-        case(HIGH):
-          new ToggleIntake(IntakeSubsystem, IntakeRollerState.SCOREHIGH);
-          break;
-        case(MID):
-          new ToggleIntake(IntakeSubsystem, IntakeRollerState.SCOREHIGH);
-          break;
-        case(LOW):
-          new ToggleIntake(IntakeSubsystem, IntakeRollerState.SCORELOW);
-          break;
-      }
-    ));*/
+    /*
+     * OperatorConstants.driverController2.rightTrigger().whileTrue(Commands.
+     * sequence(
+     * switch(getPivState()){
+     * case(HOMED):
+     * break;
+     * case(COLLECT):
+     * break;
+     * case(HIGH):
+     * new ToggleIntake(IntakeSubsystem, IntakeRollerState.SCOREHIGH);
+     * break;
+     * case(MID):
+     * new ToggleIntake(IntakeSubsystem, IntakeRollerState.SCOREHIGH);
+     * break;
+     * case(LOW):
+     * new ToggleIntake(IntakeSubsystem, IntakeRollerState.SCORELOW);
+     * break;
+     * }
+     * ));
+     */
   }
+
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
--- src/main/java/com/team6647/utils/Constants.java
+++ src/main/java/com/team6647/utils/Constants.java
@@ -5,8 +5,14 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import com.andromedalib.andromedaSwerve.andromedaModule.AndromedaModule;
 import com.andromedalib.andromedaSwerve.utils.AndromedaMap;
 import com.andromedalib.andromedaSwerve.utils.AndromedaModuleConstants;
+import com.andromedalib.andromedaSwerve.utils.AndromedaProfileConfig;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.ctre.phoenix.sensors.SensorTimeBase;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
@@ -26,16 +32,66 @@ public class Constants {
         public static final ShuffleboardTab kShuffleboardTab = Shuffleboard.getTab(kShuffleboardTabName);
     }    
 
-    public static class DriveConsants{
-        public static final AndromedaModuleConstants mod1Const = new AndromedaModuleConstants(AndromedaMap.mod1Const.steeringMotorID, AndromedaMap.mod1Const.driveMotorID, AndromedaMap.mod1Const.absCanCoderID, Rotation2d.fromDegrees(202.061));
-        public static final AndromedaModuleConstants mod2Const = new AndromedaModuleConstants(AndromedaMap.mod2Const.steeringMotorID, AndromedaMap.mod2Const.driveMotorID, AndromedaMap.mod2Const.absCanCoderID, Rotation2d.fromDegrees(41.660));
-        public static final AndromedaModuleConstants mod3Const = new AndromedaModuleConstants(AndromedaMap.mod3Const.steeringMotorID, AndromedaMap.mod3Const.driveMotorID, AndromedaMap.mod3Const.absCanCoderID, Rotation2d.fromDegrees(109.688));
-        public static final AndromedaModuleConstants mod4Const = new AndromedaModuleConstants(AndromedaMap.mod4Const.steeringMotorID, AndromedaMap.mod4Const.driveMotorID, AndromedaMap.mod4Const.absCanCoderID, Rotation2d.fromDegrees(53.613));
+    public static class DriveConstants{
+        public static final AndromedaModuleConstants mod1Const = new AndromedaModuleConstants(1, 2, AndromedaMap.mod1Const.absCanCoderID, Rotation2d.fromDegrees(342.158203));
+        public static final AndromedaModuleConstants mod2Const = new AndromedaModuleConstants(4, 5, AndromedaMap.mod2Const.absCanCoderID, Rotation2d.fromDegrees(135.615234));
+        public static final AndromedaModuleConstants mod3Const = new AndromedaModuleConstants(7, 8, AndromedaMap.mod3Const.absCanCoderID, Rotation2d.fromDegrees(248.291016));
+        public static final AndromedaModuleConstants mod4Const = new AndromedaModuleConstants(10, 11, AndromedaMap.mod4Const.absCanCoderID, Rotation2d.fromDegrees(128.144531));
+    
+        /* Base Andromeda Swerve configuration profile */
+        public static AndromedaProfileConfig neoAndromedaSwerveConfig() 
+
+            double wheelDiameter = Units.inchesToMeters(4.0);
+
+            double steeringGearRatio = ((150.0 / 7.0) / 1.0);
+            double driveGearRatio = (6.75 / 1.0);
+
+            double turningKp = 0.007;
+            double turningKi = 0.0;
+            double turningKd = 0.0;
+            double turningKf = 0.0;
+
+            double driveKp = 0.3;
+            double driveKi = 0.0;
+            double driveKd = 0.0;
+            double driveKf = 0.0;
+
+            boolean driveMotorInvert = false;
+            boolean angleMotorInvert = false;
+            boolean canCoderInvert = true;
+
+            /* Current Limiting */
+            int driveContinuousCurrentLimit = 35;
+
+            int angleContinuousCurrentLimit = 25;
+
+            /* CANCoder */
+
+            CANCoderConfiguration cancoderConfig = new CANCoderConfiguration();
+
+            cancoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+            cancoderConfig.sensorDirection = canCoderInvert;
+            cancoderConfig.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+            cancoderConfig.sensorTimeBase = SensorTimeBase.PerSecond;
+
+            /* Drive Motor */
+
+            /* Turning Motor */
+
+            return new AndromedaProfileConfig(
+                            steeringGearRatio,
+                            driveGearRatio,
+                            wheelDiameter,
+                            turningKp, turningKi, turningKd, turningKf,
+                            driveKp, driveKi, driveKd, driveKf, cancoderConfig,
+                            driveMotorInvert, angleMotorInvert,
+                            canCoderInvert, driveContinuousCurrentLimit, angleContinuousCurrentLimit);
+    }
     }
 
     public static class PivotConstants {
 
-        public static final int pivMotorID = 9;
+        public static final int pivMotorID = 15;
         public static final int intakeMotorID = 10;
 
         public static final double pivotKp = 0.1; 
@@ -72,3 +128,4 @@ public class Constants {
         public static final int beamBrakePort = 0;
     }
 }
+

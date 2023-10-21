18c1677c39fc2de4dcc2e5a02765c52283c2f4e7
MauVillarreal2005 <mauriciovillarrealrodriguez@gmail.com>
2023-10-20 20:37:43 -0700

    update

--- src/main/java/com/team6647/utils/Constants.java
+++ src/main/java/com/team6647/utils/Constants.java
@@ -3,6 +3,7 @@ package com.team6647.utils;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import com.andromedalib.andromedaSwerve.andromedaModule.AndromedaModule;
+import com.andromedalib.andromedaSwerve.andromedaModule.FalconAndromedaModule;
 import com.andromedalib.andromedaSwerve.andromedaModule.NeoAndromedaModule;
 import com.andromedalib.andromedaSwerve.utils.AndromedaMap;
 import com.andromedalib.andromedaSwerve.utils.AndromedaModuleConstants;
@@ -31,22 +32,25 @@ public class Constants {
     public static class ShuffleboardConstants {
         private static final String kShuffleboardTabName = "Team 6647";
         public static final ShuffleboardTab kShuffleboardTab = Shuffleboard.getTab(kShuffleboardTabName);
-    }    
+    }
 
-    public static class DriveConstants{
-        public static final AndromedaModuleConstants mod1Const = new AndromedaModuleConstants(1, 2, AndromedaMap.mod1Const.absCanCoderID, Rotation2d.fromDegrees(37754.339844));
-        public static final AndromedaModuleConstants mod2Const = new AndromedaModuleConstants(4, 5, AndromedaMap.mod2Const.absCanCoderID, Rotation2d.fromDegrees(38739.304688));
-        public static final AndromedaModuleConstants mod3Const = new AndromedaModuleConstants(7, 8, AndromedaMap.mod3Const.absCanCoderID, Rotation2d.fromDegrees(43186.054688));
-        public static final AndromedaModuleConstants mod4Const = new AndromedaModuleConstants(10, 11, AndromedaMap.mod4Const.absCanCoderID, Rotation2d.fromDegrees(40688.082031));
-    
+    public static class DriveConstants {
+        public static final AndromedaModuleConstants mod1Const = new AndromedaModuleConstants(1, 2, 3,
+                Rotation2d.fromDegrees(159.521));
+        public static final AndromedaModuleConstants mod2Const = new AndromedaModuleConstants(4, 5, 6,
+                Rotation2d.fromDegrees(137.109));
+        public static final AndromedaModuleConstants mod3Const = new AndromedaModuleConstants(7, 8, 9,
+                Rotation2d.fromDegrees(246.621));
+        public static final AndromedaModuleConstants mod4Const = new AndromedaModuleConstants(10, 11, 12,
+                Rotation2d.fromDegrees(127.881));
+
         /* Base Andromeda Swerve configuration profile */
-        public static AndromedaProfileConfig neoAndromedaSwerveConfig(){
-
+        public static AndromedaProfileConfig neoAndromedaSwerveConfig() {
 
             double wheelDiameter = Units.inchesToMeters(4.0);
 
-            double steeringGearRatio = ((150.0 / 7.0) / 1.0);
-            double driveGearRatio = (6.75 / 1.0);
+            double steeringGearRatio = 1 / (360.0 / (((150.0 / 7.0) / 1.0) * 42));
+            double driveGearRatio = 1 / (360.0 / ((6.75 / 1.0) * 2048.0));
 
             double turningKp = 0.007;
             double turningKi = 0.0;
@@ -81,33 +85,32 @@ public class Constants {
             /* Turning Motor */
 
             return new AndromedaProfileConfig(
-                            steeringGearRatio,
-                            driveGearRatio,
-                            wheelDiameter,
-                            turningKp, turningKi, turningKd, turningKf,
-                            driveKp, driveKi, driveKd, driveKf, cancoderConfig,
-                            driveMotorInvert, angleMotorInvert,
-                            canCoderInvert, driveContinuousCurrentLimit, angleContinuousCurrentLimit);
-    }
+                    steeringGearRatio,
+                    driveGearRatio,
+                    wheelDiameter,
+                    turningKp, turningKi, turningKd, turningKf,
+                    driveKp, driveKi, driveKd, driveKf, cancoderConfig,
+                    driveMotorInvert, angleMotorInvert,
+                    canCoderInvert, driveContinuousCurrentLimit, angleContinuousCurrentLimit);
+        }
     }
 
     public static class PivotConstants {
 
-        public static final int pivMotorID = 15;
-        public static final int intakeMotorID = 10;
+        public static final int pivMotorID = 20;
 
-        public static final double pivotKp = 0.1; 
+        public static final double pivotKp = 0.1;
         public static final double pivotKi = 0;
         public static final double pivotKd = 0;
 
         public static final double intakeSpeed = 0.3;
         public static final int beamBrakePort = 2;
 
-        public static final double intakeHomedPosition = 150;
-        public static final double intakeFloorPosition = 0;
-        public static final double intakeScoreLowPositon = 90;
-        public static final double intakeScoreMidPositon = 110;
-        public static final double intakeScoreHighPositon = 130;
+        public static final double intakeHomedPosition = 208.667;
+        public static final double intakeFloorPosition = 337.540;
+        public static final double intakeScoreLowPositon = 277.820;
+        public static final double intakeScoreMidPositon = 234.969;
+        public static final double intakeScoreHighPositon = 234.969;
 
         public static final double armEncoderPositionConversionFactor = 360;
         public static final double armEncoderZeroOffset = 150.982;
@@ -116,8 +119,7 @@ public class Constants {
 
     public static class IntakeConstants {
 
-        public static final int pivotIntakeID = 11;
-        public static final int intakeMotorID = 12;
+        public static final int intakeMotorID = 21;
 
         public static final double pivotPositionConversionFactor = 100;
         public static final double pivotZeroOffset = 90.0467753;
@@ -130,4 +132,3 @@ public class Constants {
         public static final int beamBrakePort = 0;
     }
 }
-
